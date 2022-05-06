b6ba01aecaa320e1f602ba8c6f4504301acfb7fa
Uwe Fechner <fechner@aenarete.eu>
2022-05-06 13:47:59 +0200

    Add tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,4 +5,15 @@ cd("..")
 KiteUtils.set_data_path("") 
 am = AtmosphericModel()
 
-@test calc_wind_factor(am, 6.0) ≈ 1.0
+@testset "calc_wind_factor" begin
+    @test calc_wind_factor(am, 6.0, Val{1}) ≈ 1.0
+    @test calc_wind_factor(am, 6.0, Val{2}) ≈ 1.0
+    @test calc_wind_factor(am, 6.0, Val{3}) ≈ 1.0
+    @test abs(calc_wind_factor(am, 6.0, Val{4}) - 1.0) < 1.5e-5
+    @test abs(calc_wind_factor(am, 6.0, Val{5}) - 1.0) < 1.5e-5
+    @test abs(calc_wind_factor(am, 6.0, Val{6}) - 1.0) < 1.5e-5    
+end
+
+@testset "calc_rho" begin
+    @test calc_rho(am, 0.0) ≈ am.set.rho_0
+end
